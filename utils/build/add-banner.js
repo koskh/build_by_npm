1ac5721ee8e7f2ebd892ecd72f0f4c597d5758f9
koskh <koskh@yandex.ru>
2016-03-22 16:56:43 +0500

    #build_by_npm Deploy.  Метка времени сборки.

--- utils/build/add-banner.js
+++ utils/build/add-banner.js
@@ -4,5 +4,10 @@ var pckgJson = require(process.env.PWD +'/package.json');
 
 var banner = '/* ' + pckgJson.name + ' - '+ pckgJson.config.banner +' - ' + new Date() + ' */\n';
 
+//Time marker
+var timeMarker = new Date();
+process.stderr.write(timeMarker.toString() + '   ');
+
 process.stdout.write(banner);
 process.stdin.pipe(process.stdout);
+
